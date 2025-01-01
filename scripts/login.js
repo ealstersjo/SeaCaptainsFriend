af4104f3dbdef05a8d4a5c24b32eaad2c9e1f3d7
Erik Dammer <ealstersjo@gmail.com>
2025-01-01 16:29:16 +0100

    initial commit

---
+++ scripts/login.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Kontrollera om användarnamn och lösenord är korrekta
+    if (username === "oscar" && password === "oscar") {
+      // Spara inloggningsstatus i localStorage
+      localStorage.setItem("loggedIn", "true");
+      // Redirecta till dashboard.html
+      window.location.href = "dashboard.html";
+    } else {
+      // Visa felmeddelande
+      document.getElementById("error-message").textContent =
+        "Fel användarnamn eller lösenord.";
+    }
+  });
